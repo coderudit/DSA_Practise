7f4fa4c2a47d2b875b92a0cd110bf5037eac0a01
Udit Gandhi <coder.udit@gmail.com>
2021-12-21 15:12:57 -0400

    Updated with maze paths.

--- src/com/dsa/app/level1/recursion/_13_MazePath.java
+++ src/com/dsa/app/level1/recursion/_13_MazePath.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 public class _13_MazePath {
     public static void main(String[] args) {
-        for (var item : getMazePaths(1, 1, 3, 3)) {
+        /*for (var item : getMazePaths(1, 1, 3, 3)) {
+            System.out.println(item);
+        }*/
+
+        for (var item : getMazePathsWithJumps(1, 1, 3, 3)) {
             System.out.println(item);
         }
     }
@@ -30,6 +34,58 @@ public class _13_MazePath {
         }
         for (var item : vPaths) {
             combinedList.add("v" + item);
+        }
+        return combinedList;
+    }
+
+    private static List<String> getMazePathsWithJumps(int sr, int sc, int dr, int dc) {
+        if (sr == dr && sc == dc) {
+            var finalPaths = new ArrayList<String>();
+            finalPaths.add("");
+            return finalPaths;
+        }
+
+        List<String> hPaths = new ArrayList<>();
+        List<String> vPaths = new ArrayList<>();
+        List<String> dPaths = new ArrayList<>();
+
+        if (sc != dc) {
+            for (int index = 1; index <= dc; index++) {
+                if (sc + index != dc) {
+                    hPaths = getMazePaths(sr, sc + index, dr, dc);
+                }else
+                    break;
+            }
+        }
+        if (sr != dr) {
+            for (int index = 1; index <= dr; index++) {
+                if (sr + index != dc) {
+                    vPaths = getMazePaths(sr + index, sc, dr, dc);
+                }
+                else
+                    break;
+            }
+        }
+        if (sc != dc && sr != dr) {
+            for (int index = 1; index <= dr; index++) {
+                if (sr + index != dr) {
+                    dPaths = getMazePaths(sr + index, sc + index, dr, dc);
+                }
+                else
+                    break;
+            }
+
+        }
+
+        List<String> combinedList = new ArrayList<>();
+        for (var item : hPaths) {
+            combinedList.add("h" + item);
+        }
+        for (var item : vPaths) {
+            combinedList.add("v" + item);
+        }
+        for (var item : dPaths) {
+            combinedList.add("d" + item);
         }
         return combinedList;
     }
