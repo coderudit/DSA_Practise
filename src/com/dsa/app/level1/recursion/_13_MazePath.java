cf3cc46e5a6b619ac564f353fea0d5c47d30a44d
Udit Gandhi <coder.udit@gmail.com>
2021-12-25 03:07:39 -0400

    Print maze path with jumps

--- src/com/dsa/app/level1/recursion/_13_MazePath.java
+++ src/com/dsa/app/level1/recursion/_13_MazePath.java
@@ -5,17 +5,22 @@ import java.util.List;
 
 public class _13_MazePath {
     public static void main(String[] args) {
-        for (var item : getMazePaths(1, 1, 3, 3)) {
+        /*for (var item : getMazePaths(1, 1, 3, 3)) {
+            System.out.println(item);
+        }*/
+
+        //System.out.println();
+
+        //printMazePath(1,1, 3, 3, "");
+
+        for (var item : getMazePathsWithJumps(1, 1, 3, 3)) {
             System.out.println(item);
         }
 
         System.out.println();
 
-        printMazePath(1,1, 3, 3, "");
+        printMazePathsWithJumps(1,1,3,3, "");
 
-        /*for (var item : getMazePathsWithJumps(1, 1, 2, 2)) {
-            System.out.println(item);
-        }*/
     }
 
     private static List<String> getMazePaths(int sr, int sc, int dr, int dc) {
@@ -42,19 +47,18 @@ public class _13_MazePath {
         return combinedList;
     }
 
-    private static void printMazePath(int sr, int sc, int dr, int dc, String mazePath){
-        if(sr==dr && sc==dc){
+    private static void printMazePath(int sr, int sc, int dr, int dc, String mazePath) {
+        if (sr == dr && sc == dc) {
             System.out.println(mazePath);
         }
 
-        if(sc!=dc)
-            printMazePath(sr, sc+1, dr, dc, "v" + mazePath );
+        if (sc != dc)
+            printMazePath(sr, sc + 1, dr, dc, "v" + mazePath);
 
-        if(sr != dr)
-            printMazePath(sr+1, sc, dr, dc, "h" + mazePath);
+        if (sr != dr)
+            printMazePath(sr + 1, sc, dr, dc, "h" + mazePath);
 
     }
-
 
     private static List<String> getMazePathsWithJumps(int sr, int sc, int dr, int dc) {
         if (sr == dr && sc == dc) {
@@ -73,10 +77,10 @@ public class _13_MazePath {
             for (int index = 1; index < dc; index++) {
                 if (sc + index <= dc) {
                     hPaths = getMazePathsWithJumps(sr, sc + index, dr, dc);
-                    for(var item: hPaths){
-                        combinedList.add("h"+ index+item);
+                    for (var item : hPaths) {
+                        combinedList.add("h" + index + item);
                     }
-                }else
+                } else
                     break;
             }
         }
@@ -84,11 +88,10 @@ public class _13_MazePath {
             for (int index = 1; index < dr; index++) {
                 if (sr + index <= dc) {
                     vPaths = getMazePathsWithJumps(sr + index, sc, dr, dc);
-                    for(var item: vPaths){
-                        combinedList.add("v"+ index+item);
+                    for (var item : vPaths) {
+                        combinedList.add("v" + index + item);
                     }
-                }
-                else
+                } else
                     break;
             }
         }
@@ -96,16 +99,37 @@ public class _13_MazePath {
             for (int index = 1; index < dr; index++) {
                 if (sr + index <= dr) {
                     dPaths = getMazePathsWithJumps(sr + index, sc + index, dr, dc);
-                    for(var item: dPaths){
-                        combinedList.add("d"+ index+item);
+                    for (var item : dPaths) {
+                        combinedList.add("d" + index + item);
                     }
-                }
-                else
+                } else
                     break;
             }
 
         }
 
         return combinedList;
+    }
+
+    private static void printMazePathsWithJumps(int sr, int sc, int dr, int dc, String mazePath) {
+        if (sr == dr && sc == dc) {
+            System.out.println(mazePath);
+            return;
+        }
+
+        for (int index = 1; index <= dr; index++) {
+            if (sr + index <= dr)
+                printMazePathsWithJumps(sr + index, sc, dr, dc, mazePath + "h"+index);
+        }
+
+        for (int index = 1; index <= dr; index++) {
+            if (sc + index <= dc)
+                printMazePathsWithJumps(sr, sc+ index, dr, dc, mazePath + "v"+index);
+        }
+
+        for (int index = 1; index <= dr; index++) {
+            if (sr + index <= dr && sc + index <= dc)
+                printMazePathsWithJumps(sr + index, sc + index, dr, dc, mazePath + "d"+index);
+        }
     }
 }
