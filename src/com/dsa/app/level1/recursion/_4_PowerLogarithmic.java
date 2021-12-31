589e833295169a78eb2068bedc97b9dbc8e4c447
Udit Gandhi <coder.udit@gmail.com>
2021-12-30 23:45:49 -0400

    Power logarithmic

--- src/com/dsa/app/level1/recursion/_4_PowerLogarithmic.java
+++ src/com/dsa/app/level1/recursion/_4_PowerLogarithmic.java
@@ -5,16 +5,36 @@ import java.util.Scanner;
 public class _4_PowerLogarithmic {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        int power = scanner.nextInt();
-        System.out.println("Result: " + logarithmicPower(number, power));
+        double number = 1.00000;
+        int power = -2147483648;
+        double result = myPow(number, power);
+        System.out.println(result);
     }
 
-    private static int logarithmicPower(int number, int power) {
-        if (power <= 1) {
+    public static double myPow(double x, int n) {
+        if(n==0)
+            return 1;
+        else if(n==1)
+            return x;
+        else if(x == 1){
+            return x;
+        }
+        int newN = n;
+        if(newN < 1){
+            newN = Math.abs(newN);
+        }
+        double result =  logarithmicPower(x, newN);
+        if(n < 1){
+            result = 1/result;
+        }
+        return result;
+    }
+
+    private static double logarithmicPower(double number, int power) {
+        if (power == 1) {
             return number;
         }
-        int result = logarithmicPower(number, (int) power / 2);
+        double result = logarithmicPower(number, (int) power / 2);
         result = result * result;
         if (power % 2 != 0)
             result = result * number;
