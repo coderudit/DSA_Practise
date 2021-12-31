9e5e3c758942c60b88a781cd5e92063c41c08e9b
Udit Gandhi <coder.udit@gmail.com>
2021-12-31 01:10:29 -0400

    Print permutations

--- src/com/dsa/app/level1/recursion/_14_PrintPermutations.java
+++ src/com/dsa/app/level1/recursion/_14_PrintPermutations.java
@@ -1,5 +1,19 @@
 package com.dsa.app.level1.recursion;
 
 public class _14_PrintPermutations {
+    public static void main(String[] args) {
+        printPermutations("abc", "");
+    }
 
+    private static void printPermutations(String input, String value) {
+        if (input == null || input.length() == 0) {
+            System.out.println(value);
+            return;
+        }
+
+        for (int index = 0; index < input.length(); index++) {
+            Character chosenChar = input.charAt(index);
+            printPermutations(input.substring(0, index) + input.substring(index + 1), value + chosenChar);
+        }
+    }
 }
