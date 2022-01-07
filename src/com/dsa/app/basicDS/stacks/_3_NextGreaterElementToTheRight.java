0704a1eb667a5296870de1a6b720651dee2f45a6
Udit Gandhi <coder.udit@gmail.com>
2022-01-06 23:54:17 -0400

    Next greater element to the right.

---
+++ src/com/dsa/app/basicDS/stacks/_3_NextGreaterElementToTheRight.java
@@ -0,0 +1,32 @@
+package com.dsa.app.basicDS.stacks;
+
+import java.util.Stack;
+
+public class _3_NextGreaterElementToTheRight {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 9, 3, 1, 12, 6,8, 7};
+        int[] newArr = solve(arr);
+        for(int index = 0; index < newArr.length; index++) {
+            System.out.println(newArr[index]);
+        }
+
+    }
+
+    public static int[] solve(int[] arr){
+        int[] newArr = new int[arr.length];
+        //newArr[arr.length-1] = -1;
+        Stack<Integer> stack = new Stack<Integer>();
+        for(int index = arr.length-1; index >= 0; index--){
+            while (stack.size() > 0 && stack.peek() <= arr[index]) {
+                stack.pop();
+            }
+            if(stack.isEmpty())
+                newArr[index] = -1;
+            else
+                newArr[index] = stack.peek();
+
+            stack.push(arr[index]);
+        }
+        return newArr;
+    }
+}
