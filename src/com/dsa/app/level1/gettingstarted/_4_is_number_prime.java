d8c3d3e24fed9e0bd6b4b0693c67de325ca17030
Udit Gandhi <coder.udit@gmail.com>
2022-01-26 22:32:55 -0400

    Updated prime number code.

--- src/com/dsa/app/level1/gettingstarted/_4_is_number_prime.java
+++ src/com/dsa/app/level1/gettingstarted/_4_is_number_prime.java
@@ -1,41 +1,51 @@
 package com.dsa.app.level1.gettingstarted;
 
+import java.util.Scanner;
+
 public class _4_is_number_prime {
 
-    public static boolean isNumberPrime(int number) {
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int numsCount = scanner.nextInt();
+    for(int index = 0; index < numsCount; index++){
+        int num = scanner.nextInt();
+    }
+    }
+
+    public static String isNumberPrime(int number) {
         if (number <= 1)
-            return false;
+            return "Not Prime";
         for (int index = 2; index < number; index++) {
             if (number % index == 0)
-                return false;
+                return "Not Prime";
         }
-        return true;
+        return "Prime";
     }
 
-    public static boolean isNumberPrimeSqrt(int number) {
+    public static String isNumberPrimeSqrt(int number) {
         if (number <= 1)
-            return false;
-        for (int index = 2; index <= Math.sqrt(number); index++) {
+            return "Not Prime";
+        for (int index = 2; index *index <= number; index++) {
             if (number % index == 0)
-                return false;
+                return "Not Prime";
         }
-        return true;
+        return "Prime";
     }
 
-    public static boolean isNumberPrimeSqrtOptimized(int number) {
+    public static String isNumberPrimeSqrtOptimized(int number) {
         if (number <= 1)
-            return false;
+            return "Not Prime";
         else if (number == 2)
-            return true;
+            return "Prime";
         else if (number % 2 == 0) {
             assert (number > 2);
-            return false;
+            return "Not Prime";
         }
         for (int index = 3; index <= Math.sqrt(number); index++) {
             if (number % index == 0)
-                return false;
+                return "Not Prime";
         }
-        return true;
+        return "Prime";
     }
 
     public static void sieveMethodForGettingListOfPrimes(int number){
