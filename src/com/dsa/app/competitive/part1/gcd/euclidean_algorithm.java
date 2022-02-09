abde4b2024580a034529e0d7003d8e44ce5262b5
Udit Gandhi <coder.udit@gmail.com>
2022-02-09 19:12:12 -0400

    Euclidean algorithm updated with binary gcd.

--- src/com/dsa/app/competitive/part1/gcd/euclidean_algorithm.java
+++ src/com/dsa/app/competitive/part1/gcd/euclidean_algorithm.java
@@ -28,8 +28,48 @@ public class euclidean_algorithm {
     }
 
     /**
+     * Take the highest of 2 numbers, find the remainder with other number and then, again repeat it where this time other number is divided by the remainder.
+     * It takes O(log(min(a,b))
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public int binary_gcd(int a, int b) {
+        if (a == b)
+            return a;
+
+        if (a == 0)
+            return b;
+
+        if (b == 0)
+            return a;
+
+        //If a is divisible by 2.
+        if ((a & 1) == 0) {
+            // both a and b are even
+            if ((b & 1) == 0) {
+                return gcd(a >> 1, b >> 1) << 1;
+            } else {// b is odd
+                return gcd(a >> 1, b);
+            }
+        }
+
+        // a is odd, b is even
+        if ((b & 1) == 0)
+            return gcd(a, b >> 1);
+
+        //It will come here when both a and b are odd. Subtracting 2 odd numbers will give even number.
+        if (a > b)
+            return gcd((a - b) >> 1, b);
+
+        return gcd((b - a) >> 1, a);
+    }
+
+    /**
      * LCM of 2 numbers is least number that is divisible by both the numbers. For this, find the GCD of these 2 numbers and
      * divide a * b with GCD to get the LCM.
+     *
      * @param a
      * @param b
      * @return
