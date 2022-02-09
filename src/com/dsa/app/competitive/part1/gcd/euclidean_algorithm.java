00c67b5462dd08a645240b856f4d221209b6d176
Udit Gandhi <coder.udit@gmail.com>
2022-02-09 18:45:06 -0400

    Euclidean algorithm updated.

--- src/com/dsa/app/competitive/part1/gcd/euclidean_algorithm.java
+++ src/com/dsa/app/competitive/part1/gcd/euclidean_algorithm.java
@@ -5,13 +5,16 @@ public class euclidean_algorithm {
         euclidean_algorithm obj = new euclidean_algorithm();
         //System.out.println(obj.gcd(10, 45));
         //System.out.println(obj.gcd(1, 5));
-        System.out.println(obj.gcd(1701, 3768));
+        System.out.println("GCD: " + obj.gcd(1701, 3768));
         //System.out.println(obj.gcd(10, 45));
         //System.out.println(obj.gcd(10, 45));
+
+        System.out.println("LCM: " + obj.lcm(1701, 3768));
     }
 
     /**
      * Take the highest of 2 numbers, find the remainder with other number and then, again repeat it where this time other number is divided by the remainder.
+     * It takes O(log(min(a,b))
      *
      * @param a
      * @param b
@@ -22,5 +25,16 @@ public class euclidean_algorithm {
             return b;
 
         return gcd(b % a, a);
+    }
+
+    /**
+     * LCM of 2 numbers is least number that is divisible by both the numbers. For this, find the GCD of these 2 numbers and
+     * divide a * b with GCD to get the LCM.
+     * @param a
+     * @param b
+     * @return
+     */
+    public int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
     }
 }
